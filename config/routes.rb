260f389212b66a1348de72d1eea88729df6335b8
EC2 Default User <104773179+kazuwaki@users.noreply.github.com>
2022-08-01 04:52:58 +0000

    [Add]ヘッダー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
  devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -10,9 +15,15 @@ Rails.application.routes.draw do
   }
 
   scope module: :public do
+    root to: "homes#top"
+    get '/about' => 'homes#about'
     resources :posts, only: [:index, :new, :edit, :update, :create, :destroy]
-
-
+    resource :customers do
+      get '/my_page' => 'customers#show'
+      get '/customers' => 'customers#index'
+      get '/information/edit' => 'customers#edit'
+      patch  '/information' => 'customers#update'
+    end
 
   end
 
