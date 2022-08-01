ca10dbcb31616f411062eda71bf8c2d285e63bcf
EC2 Default User <104773179+kazuwaki@users.noreply.github.com>
2022-08-01 12:41:04 +0000

    [Add]投稿機能の画像追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_31_120655) do
+ActiveRecord::Schema.define(version: 2022_08_01_105837) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -75,7 +75,7 @@ ActiveRecord::Schema.define(version: 2022_07_31_120655) do
   create_table "posts", force: :cascade do |t|
     t.string "name", null: false
     t.string "body", null: false
-    t.integer "user_id", null: false
+    t.integer "customer_id"
     t.integer "type_id", null: false
     t.integer "alcohol_id", null: false
     t.datetime "created_at", precision: 6, null: false
