de88a5a6b1a4697d6d57b7a70d5a55ebfdc35ab6
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-08-02 14:11:40 +0000

    [Add]いいね機能非同期

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -3,13 +3,11 @@ class Public::LikesController < ApplicationController
     @post = Post.find(params[:post_id])
     like = current_customer.likes.new(post_id: @post.id)
     like.save
-    redirect_to request.referer
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     like = current_customer.likes.find_by(post_id: @post.id)
     like.destroy
-    redirect_to request.referer
   end
 end
