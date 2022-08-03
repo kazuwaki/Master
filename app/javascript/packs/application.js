79b8124d1f27e0c125511f342537b63a9f8fa7d7
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-08-03 14:31:48 +0000

    [Add]診断チャート作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery";
+import "./index"
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
