eff9b1e46fd429f7560fc6cbbe9b8ece58a3e3ee
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-08-04 09:08:59 +0000

    [Add]コメントのバリデーション

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -3,7 +3,9 @@ class Public::PostCommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = current_customer.post_comments.new(post_comment_params)
     @comment.post_id = @post.id
-    @comment.save
+    unless @comment.save
+      render :index
+    end
   end
 
   def destroy
