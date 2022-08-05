8e0e1fd170da25b459b292c6bff8fefd07501079
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-08-05 13:37:47 +0000

    [Add]新規投稿画面のレイアウト

--- app/models/post.rb
+++ app/models/post.rb
@@ -25,7 +25,6 @@ class Post < ApplicationRecord
     end
   end
 
-
   def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -33,4 +32,9 @@ class Post < ApplicationRecord
     end
     image.variant(resize_to_limit: [width, height]).processed
   end
+  
+  
+  
+  
+  
 end
