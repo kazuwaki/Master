f50362a11e17a10b183e7b8e26822ee73de2c0b6
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-08-08 08:35:48 +0000

    [Add]Rspecタイプの確認

---
+++ spec/system/types_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe '投稿のテスト' do
+  let!(:type) { create(:type, name:'hoge') }
+  describe 'タイプ画面(admin_types_path)のテスト' do
+    before do
+      visit admin_types_path
+    end
+    context '表示の確認' do
+      it 'タイプ画面(top_path)に「タイプ種類を作成」が表示されているか' do
+        expect(page).to have_content 'タイプ種類を作成'
+      end
+      it 'admin_types_pathが"/admin/types"であるか' do
+        expect(current_path).to eq('/admin/types')
+      end
+    end
+  end
+end
