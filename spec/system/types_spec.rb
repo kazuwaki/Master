dfe0c98fa5d6f5e804d4b056ae56213b79ec5371
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-08-08 14:45:37 +0000

    [Add]TypeのRspecを追加

--- spec/system/types_spec.rb
+++ spec/system/types_spec.rb
@@ -15,6 +15,32 @@ describe 'Type投稿のテスト' do
       it 'admin_types_pathが"/admin/types"であるか' do
         expect(current_path).to eq('/admin/types')
       end
+      it '「編集」が表示されている' do
+        expect(page).to have_content '編集'
+      end
+      it '「新規登録」が表示されている' do
+        expect(page).to have_content '新規登録'
+      end
+    end
+    describe '編集画面のテスト' do
+      before do
+        visit edit_admin_type_path(type)
+      end
+      context '表示の確認' do
+        it '編集前のnameが表示(セット)されている' do
+          expect(page).to have_field 'type[name]', with: type.name
+        end
+        it '更新内容を保存ボタンが表示される' do
+          expect(page).to have_button '更新内容を保存'
+        end
+      end
+      context '更新処理に関するテスト' do
+        it '更新後のリダイレクト先は正しいか' do
+          fill_in 'type[name]', with: Faker::Lorem.characters(number:5)
+          click_button '更新内容を保存'
+          expect(page).to have_current_path admin_types_path
+        end
+      end
     end
   end
 end
