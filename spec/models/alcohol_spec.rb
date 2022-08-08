d8824e82da717c8e530c4ba7db1e061b5153636a
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-08-08 09:24:58 +0000

    [Add]アルコール作成画面のRspec

--- spec/models/alcohol_spec.rb
+++ spec/models/alcohol_spec.rb
@@ -2,8 +2,8 @@
 
 require 'rails_helper'
 
-describe 'Typeモデルのテスト' do
+describe 'Alcoholモデルのテスト' do
   it "有効な投稿内容の場合は保存されるか" do
-    expect(FactoryBot.build(:type)).to be_valid
+    expect(FactoryBot.build(:alcohol)).to be_valid
   end
 end
--- spec/system/types_spec.rb
+++ spec/system/types_spec.rb
@@ -2,14 +2,14 @@
 
 require 'rails_helper'
 
-describe '投稿のテスト' do
+describe 'Type投稿のテスト' do
   let!(:type) { create(:type, name:'hoge') }
   describe 'タイプ画面(admin_types_path)のテスト' do
     before do
       visit admin_types_path
     end
     context '表示の確認' do
-      it 'タイプ画面(top_path)に「タイプ種類を作成」が表示されているか' do
+      it 'タイプ画面(admin_types_path)に「タイプ種類を作成」が表示されているか' do
         expect(page).to have_content 'タイプ種類を作成'
       end
       it 'admin_types_pathが"/admin/types"であるか' do
