6fe06bf44a3506f9084a7b31de39c35c99f32b1f
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-08-09 01:52:01 +0000

    [Add]ユーザーのモデルテスト

--- spec/models/type_spec.rb
+++ spec/models/type_spec.rb
@@ -6,4 +6,17 @@ describe 'Typeモデルのテスト' do
   it "有効な投稿内容の場合は保存されるか" do
     expect(FactoryBot.build(:type)).to be_valid
   end
+  describe 'バリデーションのテスト' do
+    subject { type.valid? }
+
+    let!(:other_type) { create(:type) }
+    let(:type) { build(:type) }
+
+    context 'nameカラム' do
+      it '空欄でないこと' do
+        type.name = ''
+        is_expected.to eq false
+      end
+    end 
+  end
 end
