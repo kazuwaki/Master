619270d05604e1aedb34f39c9c3b2c70f6a5923f
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-08-29 08:29:17 +0000

    [Add]Adminのバグ修正

--- app/controllers/admin/time_line_comments_controller.rb
+++ app/controllers/admin/time_line_comments_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Admin::TimeLineCommentsController < ApplicationController
+  def destroy
+    @time_line_comment = TimeLineComment.find(params[:id])
+    @time_line_comment.destroy
+    redirect_to admin_time_lines_path
+  end
 end
