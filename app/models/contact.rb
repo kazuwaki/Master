28dec9d2d8d25fb0686aa4727c244bac5cb4b3dc
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-08-31 13:58:36 +0000

    [Add]お問い合わせ機能バリデーション

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,8 @@
 
 class Contact < ApplicationRecord
   enum subject: { other: 0, work: 1, recruit: 2, method: 3 }
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :phone_number, presence: true
+  validates :message, presence: true
 end
